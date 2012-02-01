0a62f6a48eb5cab8dabae2996738e8c0186789cb
Navin Kabra <navin@smriti.com>
2012-02-01 17:28:11 +0530

    Added dry_run option

--- __init__.py
+++ __init__.py
@@ -5,11 +5,20 @@ import subprocess
 class BadProcess(Exception):
     pass
 
-def restart(command, pidfile):
-    command[0:0] = ['/usr/bin/env', 'daemonize', '-p', pidfile]
-    subprocess.Popen(command)
+def restart(command, pidfile, chdir, dry_run=False):
+    daemonize = ['/usr/bin/env', 'daemonize', '-p', pidfile]
+    if chdir:
+        daemonize.extend(['-c', chdir])
 
-def handle(program, command, pidfile, verifier, **kwargs):
+    command[0:0] = daemonize
+    if not dry_run:
+        subprocess.Popen(command)
+    else:
+        print 'Re-Start:', command
+
+def handle(program, command, pidfile, verifier,
+           dry_run=False,
+           **kwargs):
     pidfile = pidfile.format(program=program)
     verifier = verifier.format(program=program)
     # replace {program} in pidfile/verifier name with actual program name
@@ -22,15 +31,16 @@ def handle(program, command, pidfile, verifier, **kwargs):
         if verifier not in pcmd:
             raise BadProcess(verifier + '::' + pcmd)
     except (IOError, psutil.NoSuchProcess, BadProcess):
-        restart(command, pidfile)
+        restart(command, pidfile, kwargs.get('chdir'), dry_run=dry_run)
 
 
-def handle_all(config_file):
+def handle_all(config_file, dry_run=False):
     c = ConfigParser.SafeConfigParser()
     c.read(config_file)
 
     for section in c.sections():
         handle(program=section,
+               dry_run=dry_run,
                **dict((o, c.get(section, o)) for o in c.options(section)))
 
 
@@ -39,9 +49,10 @@ if __name__ == '__main__':
     from os.path import expanduser, exists
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config-file', default='~/pyrestarter.cfg')
+    parser.add_argument('-n', '--dry-run', action='store_true', default=False)
     args = parser.parse_args()
     config_file = expanduser(args.config_file)
     if not exists(config_file):
         print 'No such file {0}'.format(config_file)
         exit(1)
-    handle_all(config_file)
+    handle_all(config_file, dry_run=args.dry_run)
